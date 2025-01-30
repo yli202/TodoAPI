66f0f584c3909285ac02347da7a57f13507b2174
TasdidKabirAdil <tadil@my.centennialcollege.ca>
2025-01-30 15:01:50 -0500

    added more weather summaries

--- TodoAPI/Controllers/WeatherForecastController.cs
+++ TodoAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TodoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy", "Snowing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
