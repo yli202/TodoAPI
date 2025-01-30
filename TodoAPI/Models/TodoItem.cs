002553b3ab9423d6334110380e8edb1c409a4427
yadaBrown <ybrown4@my.centennialcollege.ca>
2025-01-30 15:02:07 -0500

    Fixed Issue <Update data items of TodoItems> Added: Quiz#2 to TODOItems.cs

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -5,5 +5,14 @@
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+
+        TodoItem newItem = new TodoItem
+        {
+            Id = 1,
+            Name = "Quiz 2",
+            IsComplete = false
+
+        };
     }
+
 }
