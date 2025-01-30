a87004eb2c4aa1aa070961ede7e164d033595905
sebs6699 <144267312+sebs6699@users.noreply.github.com>
2025-01-30 15:20:45 -0500

    Update TodoItem.cs
    Fix #<1>: Added new field to TodoItem

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -5,5 +5,7 @@
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Description { get; set; } // New field
+
     }
 }
